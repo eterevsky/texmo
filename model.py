3e324e8a3aed8ef20d16cadcccd84cc0f4ca8c0a
Oleg Eterevsky <oleg@eterevsky.com>
2022-04-03 13:23:28 +0200

    Move Model to a separate module.

---
+++ model.py
@@ -0,0 +1,56 @@
+NCHAR = 256  # Considering characters with codes 0..NCHAR
+
+
+class Model(object):
+    def init_params(self, key):
+        """Initializes the params object.
+
+        Args:
+            key: JAX pseudorandom key, PRNGKey
+        """
+        return {}
+
+    def init_state(self):
+        """Initialize the state object."""
+        return None
+
+    def step(self, params, state, c):
+        """One forward step in the recurrent network.
+
+        Args:
+            params: dictionary with the model parameters
+            state: array with the internal state (initialized by init_state or returned from the
+                previous step)
+            x: a single character represented as a one-of. shape = (256,)
+
+        Returns:
+            (new state, an output 1D vector of size NCHAR that after softmax will produce character
+            probabilities.)
+        """
+        pass
+
+    def step_prob(self, params, state, c, temperature=1.0):
+        """Runs one step of the model and calculate the final probabilities.
+
+        Arguments are the same as in step().
+
+        Returns:
+            (new state, a 1D vector of size NCHAR with probabilities of characters)
+        """
+        state, out = self.step(params, state, c)
+        out_softmax = jax.nn.softmax(out / temperature)
+        return state, out_softmax
+
+    def loss(self, params, x):
+        _, y = jax.lax.scan(lambda s, c: self.step(params, s, c), self.init_state(), x)
+        # print('loss', x.shape, y.shape)
+
+        return optax.softmax_cross_entropy(y[:-1,:], x[1:,:])
+
+    def params_loss(self, params):
+        if not params: return 0
+        s = 0
+        for v in params.values():
+            s += jnp.average(v * v)
+
+        return s

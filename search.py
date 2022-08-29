07b9498624a5ba6003bf93004e529580eb1ff544
Oleg Eterevsky <oleg@eterevsky.com>
2022-08-29 20:28:25 +0200

    Adjust LR together with batch size

--- search.py
+++ search.py
@@ -203,15 +203,15 @@ def conf_neighbors(conf, max_weights=None):
     #         sample_len=conf.sample_len // 2, batch=conf.batch * 2
     #     )
 
+    ilr = LRS.index(conf.lr)
+    if ilr > 0:
+        yield conf._replace(lr=LRS[ilr - 1], batch=conf.batch * 2)
+    if ilr < len(LRS) - 1 and conf.batch > 1:
+        yield conf._replace(lr=LRS[ilr + 1], batch=conf.batch // 2)
+
     yield conf._replace(batch=conf.batch * 2)
     if conf.batch > 1:
         yield conf._replace(batch=conf.batch // 2)
-
-    ilr = LRS.index(conf.lr)
-    if ilr > 0:
-        yield conf._replace(lr=LRS[ilr - 1])
-    if ilr < len(LRS) - 1:
-        yield conf._replace(lr=LRS[ilr + 1])
 
 
 class ConfResults(object):

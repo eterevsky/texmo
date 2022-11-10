66ac06b2a8882f165f9b89a04a3a327509e83c5a
Oleg Eterevsky <oleg@eterevsky.com>
2022-11-10 09:19:45 +0100

    log-4090

--- texmo/resultdb.py
+++ texmo/resultdb.py
@@ -117,8 +117,6 @@ class ResultDB(object):
         if template is None:
             template = Template()
         
-        print("get_confs_runs")
-
         conditions = []
         bindings = []
 
@@ -151,9 +149,6 @@ class ResultDB(object):
             f"SELECT {CONF_FIELDS}, run.loss FROM conf, run "
             + f"WHERE conf.id = run.conf_id {condition}"
         )
-
-        print(query)
-        print(bindings)
 
         cur = self._db.execute(query, bindings)
 

7856ce113582d8fb5a5ae20e2848a43bf1e7a14a
Oleg Eterevsky <oleg@eterevsky.com>
2022-11-18 19:16:16 +0100

    Remove old modules and update tests.

--- tests/test_resultdb.py
+++ tests/test_resultdb.py
@@ -3,17 +3,17 @@ from unittest import main, TestCase
 from texmo.configuration import Configuration, Template
 from texmo.record import TrainingRecord
 from texmo.resultdb import ResultDB
-from texmo.spec import ModelSpec
+from texmo.model2 import build_model
 
 
-def create_record(spec, loss, batch=256, reg=0.1):
+def create_record(spec, loss, batch=256, reg=0.125):
     return TrainingRecord(
         timestamp="2022-02-02T02:02:02",
         model_spec=spec,
         weights=1024,
         steps=123,
         train_time_s=8,
-        learning_rate=0.2,
+        learning_rate=0.25,
         regularization=reg,
         train_sample_len=128,
         train_batch=batch,
@@ -55,7 +55,7 @@ class ResultDBTest(TestCase):
         self.assertEqual(cur.fetchall()[0][0], 2)
 
     def test_skip_invalid(self):
-        self.db.add_record(create_record("attention.16", 123), skip_invalid=True)
+        self.db.add_record(create_record("gru.16.relu", 123), skip_invalid=True)
         self.db.add_record(
             create_record("dense.16.tanh", 3.321, batch=127), skip_invalid=True
         )
@@ -72,22 +72,22 @@ class ResultDBTest(TestCase):
         self.db.add_record(create_record("dense.16.relu", 3.321))
         self.db.add_record(create_record("dense.16.relu", 3.321, batch=64))
         self.db.add_record(
-            create_record("dense.16.relu", 3.321, batch=64, reg=0.001)
+            create_record("dense.16.relu", 3.321, batch=64, reg=0.03125)
         )
 
         confs = self.db.get_confs_runs(Template(sample_len=128, init_scale=1.0))
         all = set((conf._replace(id=None), loss) for conf, loss in confs)
 
-        spec_tanh = ModelSpec.parse("dense.16.tanh")
-        spec_relu = ModelSpec.parse("dense.16.relu")
+        spec_tanh = build_model("dense.16.tanh")
+        spec_relu = build_model("dense.16.relu")
 
         conf = Configuration(
             None,
             spec_relu,
-            lr=0.2,
+            lr=0.25,
             sample_len=128,
             batch=256,
-            regularization=0.1,
+            regularization=0.125,
             init_scale=1.0,
             t=8,
         )
@@ -95,22 +95,22 @@ class ResultDBTest(TestCase):
         self.assertEqual(
             all,
             {
-                (conf._replace(spec=spec_tanh), 3.123),
-                (conf._replace(spec=spec_tanh), 3.321),
+                (conf._replace(model=spec_tanh), 3.123),
+                (conf._replace(model=spec_tanh), 3.321),
                 (conf, 3.321),
                 (conf._replace(batch=64), 3.321),
-                (conf._replace(batch=64, regularization=0.001), 3.321),
+                (conf._replace(batch=64, regularization=0.03125), 3.321),
             },
         )
 
-        confs = self.db.get_confs_runs(Template(sample_len=128, init_scale=1.0, regularization=0.1))
+        confs = self.db.get_confs_runs(Template(sample_len=128, init_scale=1.0, regularization=0.125))
         subset1 = set((conf._replace(id=None), loss) for conf, loss in confs)
 
         self.assertEqual(
             subset1,
             {
-                (conf._replace(spec=spec_tanh), 3.123),
-                (conf._replace(spec=spec_tanh), 3.321),
+                (conf._replace(model=spec_tanh), 3.123),
+                (conf._replace(model=spec_tanh), 3.321),
                 (conf, 3.321),
                 (conf._replace(batch=64), 3.321),
             },
@@ -122,8 +122,8 @@ class ResultDBTest(TestCase):
         self.assertEqual(
             subset2,
             {
-                (conf._replace(spec=spec_tanh), 3.123),
-                (conf._replace(spec=spec_tanh), 3.321),
+                (conf._replace(model=spec_tanh), 3.123),
+                (conf._replace(model=spec_tanh), 3.321),
                 (conf, 3.321),
             },
         )

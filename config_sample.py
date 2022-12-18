c5925379dd643992ef069d5c2266ea29984986bc
Oleg Eterevsky <oleg@eterevsky.com>
2022-12-18 10:31:14 +0100

    Started a config module for common arguments.

---
+++ config_sample.py
@@ -0,0 +1,8 @@
+# Default values for a number of common command-line arguments.
+
+import os
+
+DIR = os.path.dirname(__file__)
+
+DB = os.path.join(DIR, "results/db.sqlite")
+DATA = os.path.join(DIR, "data.txt")

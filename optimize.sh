0a74b9f8737a308b90c762a5a00c80705b1ddc94
Oleg Eterevsky <oleg@eterevsky.com>
2023-05-24 20:15:36 +0200

    minor

--- optimize.sh
+++ optimize.sh
@@ -2,15 +2,15 @@
 
 cargo build --release
 
-target/release/texmo -d data/books3.txt optimize-tokens -n 32 -f 1 --processing= --initial-size=108362047381 -i tokens/tokens16_raw_f1.json -o tokens/tokens32_raw_f1.json
-target/release/texmo -d data/books3.txt optimize-tokens -n 32 -f 2 --processing= --initial-size=108362047381 -i tokens/tokens16_raw_f2.json -o tokens/tokens32_raw_f2.json
-target/release/texmo -d data/books3.txt optimize-tokens -n 32 -f 4 --processing= --initial-size=108362047381 -i tokens/tokens16_raw_f4.json -o tokens/tokens32_raw_f4.json
+# target/release/texmo -d data/books3.txt optimize-tokens -n 64 -f 1 --processing= --initial-size=108362047381 -i tokens/tokens32_raw_f1.json -o tokens/tokens64_raw_f1.json
+target/release/texmo -d data/books3.txt optimize-tokens -n 64 -f 2 --processing= --initial-size=108362047381 -i tokens/tokens32_raw_f2.json -o tokens/tokens64_raw_f2.json
+target/release/texmo -d data/books3.txt optimize-tokens -n 64 -f 4 --processing= --initial-size=108362047381 -i tokens/tokens32_raw_f4.json -o tokens/tokens64_raw_f4.json
 
-target/release/texmo -d data/books3_caps.txt optimize-tokens -n 32 -f 1 --processing=caps --initial-size=108362047381 -i tokens/tokens16_caps_f1.json -o tokens/tokens32_caps_f1.json
-target/release/texmo -d data/books3_caps.txt optimize-tokens -n 32 -f 2 --processing=caps --initial-size=108362047381 -i tokens/tokens16_caps_f2.json -o tokens/tokens32_caps_f2.json
-target/release/texmo -d data/books3_caps.txt optimize-tokens -n 32 -f 4 --processing=caps --initial-size=108362047381 -i tokens/tokens16_caps_f4.json -o tokens/tokens32_caps_f4.json
+# target/release/texmo -d data/books3_caps.txt optimize-tokens -n 64 -f 1 --processing=caps --initial-size=108362047381 -i tokens/tokens32_caps_f1.json -o tokens/tokens64_caps_f1.json
+target/release/texmo -d data/books3_caps.txt optimize-tokens -n 64 -f 2 --processing=caps --initial-size=108362047381 -i tokens/tokens32_caps_f2.json -o tokens/tokens64_caps_f2.json
+target/release/texmo -d data/books3_caps.txt optimize-tokens -n 64 -f 4 --processing=caps --initial-size=108362047381 -i tokens/tokens32_caps_f4.json -o tokens/tokens64_caps_f4.json
 
-target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 32 -f 1 --processing=caps,words --initial-size=108362047381 -i tokens/tokens16_capswords_f1.json -o tokens/tokens32_capswords_f1.json
-target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 32 -f 2 --processing=caps,words --initial-size=108362047381 -i tokens/tokens16_capswords_f2.json -o tokens/tokens32_capswords_f2.json
-target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 32 -f 4 --processing=caps,words --initial-size=108362047381 -i tokens/tokens16_capswords_f4.json -o tokens/tokens32_capswords_f4.json
+# target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 64 -f 1 --processing=caps,words --initial-size=108362047381 -i tokens/tokens32_capswords_f1.json -o tokens/tokens64_capswords_f1.json
+target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 64 -f 2 --processing=caps,words --initial-size=108362047381 -i tokens/tokens32_capswords_f2.json -o tokens/tokens64_capswords_f2.json
+target/release/texmo -d data/books3_caps_words.txt optimize-tokens -n 64 -f 4 --processing=caps,words --initial-size=108362047381 -i tokens/tokens32_capswords_f4.json -o tokens/tokens64_capswords_f4.json
 

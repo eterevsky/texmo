1d8887871accd0f85cca92c507abcf87e2604262
Oleg Eterevsky <oleg@eterevsky.com>
2023-05-28 18:09:11 +0200

    Start adding tokenization to generating training samples in texmo

--- texmo.py
+++ texmo.py
@@ -13,18 +13,18 @@ def parse_args():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command", help="command to be executed")
 
-    parser_dataset_sample = subparsers.add_parser(
-        "dataset_sample", help="generate a typical sample of training data"
+    parser_sample = subparsers.add_parser(
+        "sample", help="generate a typical sample of training data"
     )
-    dataset.init_args(parser_dataset_sample)
+    dataset.init_args(parser_sample)
 
     parser_count_bytes = subparsers.add_parser(
         "small_tokens", help="generate a set of sub-byte tokens"
     )
     small_tokens.init_args(parser_count_bytes)
 
-    help_parser = subparsers.add_parser("help", help="Display help information")
-    help_parser.set_defaults(func=lambda _: help(parser), parser=parser)
+    parser_help = subparsers.add_parser("help", help="Display help information")
+    parser_help.set_defaults(func=lambda _: help(parser), parser=parser)
 
     args = parser.parse_args()
     if args.command is None:

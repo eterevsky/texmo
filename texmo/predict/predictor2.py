dbd4c18d0a3354839df303255f262cb6f075cb5d
Oleg Eterevsky <oleg@eterevsky.com>
2023-07-18 04:04:32 -0500

    Fix merge and fix taking a log(0).

--- texmo/predict/predictor2.py
+++ texmo/predict/predictor2.py
@@ -4,13 +4,9 @@ import math
 import numpy as np
 from typing import Optional, Iterable
 
-<<<<<<< HEAD
 import numpy as np
 
 from ..configuration import conf_tokens_name, Configuration, conf_to_string
-=======
-from ..configuration import conf_tokens_name, Configuration
->>>>>>> ebdad1e9ca24628380679fad40f60ef188bd3716
 from ..record import TrainingRecord
 from ..resultdb import ResultDB
 from .loss_predictor_flat import LossPredictorFlat
@@ -37,11 +33,8 @@ class Predictor2(object):
     ):
         self._sample_timing = SampleTiming(sample_timing_path)
         self._train_timing = TrainTiming(train_timing_path)
-<<<<<<< HEAD
         # Applies to timing models.
         self._samples_till_next_train = 0
-=======
->>>>>>> ebdad1e9ca24628380679fad40f60ef188bd3716
         self._loss_predictor = LossPredictorFlat(result_db)
 
     def add_record(self, record: TrainingRecord):
@@ -78,7 +71,6 @@ class Predictor2(object):
             f"Prediction: steps {steps}, sample {sample_pred:.2f} ms, step {first_pred:.2f}|{avg_pred:.2f} ms"
         )
 
-<<<<<<< HEAD
     def predict(self, confs: list[Configuration], verbose: bool = False) -> list[float]:
         steps = []
         for conf in confs:
@@ -227,54 +219,3 @@ def init_args(parser: argparse.ArgumentParser):
     )
 
     parser.set_defaults(func=main)
-=======
-        self._sample_timing.add_sample_latency(
-            token_set_name, conf.sample_len, conf.batch, record.avg_sample_time
-        )
-        self._train_timing.add_step_latency(
-            conf, record.first_step_time, record.avg_step_time
-        )
-        self._loss_predictor.count_record()
-
-    def predict_steps(self, conf: Configuration, debug: bool = False) -> Optional[int]:
-        token_set_name = conf_tokens_name(conf)
-        sample_pred = self._sample_timing.predict(
-            token_set_name, conf.sample_len, conf.batch
-        )
-        first_pred, avg_pred = self._train_timing.predict(conf)
-
-        if first_pred > conf.t:
-            steps = None
-        else:
-            avg_step = max(avg_pred, sample_pred)
-
-            steps = math.ceil((conf.t - first_pred) / avg_step) + 1
-
-        if debug:
-            sample_pred *= 1000
-            first_pred *= 1000
-            avg_pred *= 1000
-
-            logging.info(
-                f"Prediction: steps {steps}, sample {sample_pred:.2f} ms, step {first_pred:.2f}|{avg_pred:.2f} ms"
-            )
-        
-        return steps
-
-    def predict_loss(self, confs: Iterable[Configuration]) -> Iterable[tuple[Configuration, float]]:
-        if not isinstance(confs, list):
-            confs = list(confs)
-        steps = [self.predict_steps(conf) for conf in confs]
-
-        good_confs = []
-        good_steps = []
-
-        for conf, steps in zip(confs, steps):
-            if steps is None:
-                yield conf, None
-            else:
-                good_confs.append(conf)
-                good_steps.append(steps)
-
-        losses = self._loss_predictor.predict(good_confs, good_steps)
->>>>>>> ebdad1e9ca24628380679fad40f60ef188bd3716

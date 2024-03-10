9168bb2c9642c402bfc22a03834ec69843a7a320
Oleg Eterevsky <oleg@eterevsky.com>
2024-03-11 00:12:48 +0100

    Change the order of conf neighbors

--- texmo/configuration2.py
+++ texmo/configuration2.py
@@ -114,19 +114,19 @@ class Configuration2(object):
         return self.tokenizer.tokenset.name
 
     def neighbors(self) -> Iterable["Configuration2"]:
-        for model in self.model.neighbors():
-            yield self.replace(model=model)
-        yield self.replace(lr=self.lr / 2)
-        yield self.replace(lr=self.lr * 2)
-        if self.length > 1:
-            yield self.replace(length=self.length // 2)
-        yield self.replace(length=self.length * 2)
-        if self.batch > 1:
-            yield self.replace(batch=self.batch // 2)
-        yield self.replace(batch=self.batch * 2)
         if self.steps > 2:
             yield self.replace(steps=self.steps // 2)
         yield self.replace(steps=self.steps * 2)
+        yield self.replace(lr=self.lr / 2)
+        yield self.replace(lr=self.lr * 2)
+        if self.batch > 1:
+            yield self.replace(batch=self.batch // 2)
+        yield self.replace(batch=self.batch * 2)
+        if self.length > 1:
+            yield self.replace(length=self.length // 2)
+        yield self.replace(length=self.length * 2)
+        for model in self.model.neighbors():
+            yield self.replace(model=model)
 
 
 class Bounds(object):

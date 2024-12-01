f1175eed017c33650a5e41a10021cb2fd09ed74d
Oleg Eterevsky <oleg@eterevsky.com>
2024-12-01 13:35:01 +0100

    Formatting

--- texmo/manager.py
+++ texmo/manager.py
@@ -1,14 +1,10 @@
-import csv
 import json
 import logging
 import math
 import os
-import time
 from copy import copy
-from datetime import datetime
-from typing import Optional
 from time import perf_counter
-from statistics import mean
+from typing import Optional
 
 import jax
 import jax.numpy as jnp
@@ -17,15 +13,13 @@ import optax
 from jaxlib.xla_extension import XlaRuntimeError
 
 from . import latency
-from .common import INF, ttoa3, itoa3
+from .common import INF, ttoa3
 from .configuration2 import Configuration2
 from .dataset import DataSet, DataSetWrapper
-
 from .model3 import Model3, Weights
-from .prng import Rng
-from .record import TrainingRecord
-from .run import Run
 from .predict import LossTrend
+from .prng import Rng
+from .run import Run
 from .tokens import Tokenizer
 
 LOG2 = 1 / math.log(2)
@@ -81,12 +75,16 @@ class Manager(object):
         pre_training: Optional[list] = None,
     ):
         self._rng: Rng = Rng()
-        assert isinstance(system, str), f"`system` must be a string, got {system}"
+        assert isinstance(
+            system, str
+        ), f'`system` must be a string, got {system}'
         self._system: str = system
         assert isinstance(conf, Configuration2)
         self.conf: Configuration2 = conf
 
-        assert isinstance(dataset, DataSet) or isinstance(dataset, DataSetWrapper)
+        assert isinstance(dataset, DataSet) or isinstance(
+            dataset, DataSetWrapper
+        )
         self.dataset: DataSet = dataset
 
         if weights is None:
@@ -128,8 +126,8 @@ class Manager(object):
 
     def save(self, dir):
         model_name = self.name()
-        model_name = model_name.replace("|", "!")
-        path = os.path.join(dir, f"{model_name}.json")
+        model_name = model_name.replace('|', '!')
+        path = os.path.join(dir, f'{model_name}.json')
 
         weights = self.serialize_weights(self.weights)
 
@@ -141,19 +139,19 @@ class Manager(object):
             training = []
         training.append(
             {
-                "conf": self.conf.to_dict(),
-                "run": self.run.to_dict(),
+                'conf': self.conf.to_dict(),
+                'run': self.run.to_dict(),
             }
         )
 
         data = {
-            "conf": self.conf.to_dict(),
-            "training": training,
-            "weights": weights,
+            'conf': self.conf.to_dict(),
+            'training': training,
+            'weights': weights,
         }
 
-        print(f"Saving model to {path}")
-        with open(path, "w") as f:
+        print(f'Saving model to {path}')
+        with open(path, 'w') as f:
             json.dump(data, f, indent=2)
 
     @staticmethod
@@ -161,14 +159,14 @@ class Manager(object):
         with open(path) as f:
             spec = json.load(f)
 
-        conf = Configuration2.from_dict(spec["conf"])
-        weights = deserialize_weights(spec["weights"])
+        conf = Configuration2.from_dict(spec['conf'])
+        weights = deserialize_weights(spec['weights'])
 
         manager = Manager(
             conf,
             token_set=token_set,
             weights=weights,
-            pre_training=spec["training"],
+            pre_training=spec['training'],
         )
 
         return manager
@@ -218,7 +216,7 @@ class Manager(object):
         #     )
 
     def init(self, quiet=False, training=True):
-        logging.info(f"Conf: {self.conf}")
+        logging.info(f'Conf: {self.conf}')
 
         if self.train_from == 0:
             self._loss_avg = self.model.loss_batch
@@ -229,10 +227,10 @@ class Manager(object):
 
         if training:
             if not quiet:
-                logging.info("Creating loss_grad")
+                logging.info('Creating loss_grad')
             self._loss_grad = jax.jit(jax.value_and_grad(self._loss_avg))
             if not quiet:
-                logging.info("Creating optimizer")
+                logging.info('Creating optimizer')
             mask_bias = lambda tree: jax.tree_util.tree_map(
                 lambda g: len(g.shape) > 1, tree
             )
@@ -241,7 +239,9 @@ class Manager(object):
                 optax.adamw(self.conf.lr, mask=mask_bias, weight_decay=0.01),
             )
 
-            self.opt_state = self.optimizer.init(self.weights[self.train_from :])
+            self.opt_state = self.optimizer.init(
+                self.weights[self.train_from :]
+            )
             self.run = Run(loss_trend=LossTrend(), system=self._system)
             self._init_training_data()
         else:
@@ -256,7 +256,7 @@ class Manager(object):
 
         shards = 4
         while shards <= xs.shape[0]:
-            logging.info(f"Evaluating with {shards} batches")
+            logging.info(f'Evaluating with {shards} batches')
             shard_size = xs.shape[0] // shards
 
             loss = 0
@@ -286,12 +286,14 @@ class Manager(object):
         # TODO: When we can't run eval with forward, we could just run it
         # step-by-step.
 
-        logging.info("Can't evaluate the model even with shards 1. Assuming INF loss.")
+        logging.info(
+            "Can't evaluate the model even with shards 1. Assuming INF loss."
+        )
         return INF
 
     def eval(self) -> float:
         """Evaluate a model on a random sample from the training data."""
-        with latency.timer("Manager.eval"):
+        with latency.timer('Manager.eval'):
             batch, lengths = self.dataset.sample_bytes(
                 nbytes=self.test_sample_len,
                 batch=self.test_batch,
@@ -308,7 +310,9 @@ class Manager(object):
 
         state = self.model.init_state(self.weights)
         state, _ = jax.lax.scan(
-            lambda s, c: self.model.step(self.weights, s, c), state, prefix[:-1]
+            lambda s, c: self.model.step(self.weights, s, c),
+            state,
+            prefix[:-1],
         )
 
         out = []
@@ -351,7 +355,7 @@ class Manager(object):
         return loss / (xs.shape[0] * xs.shape[1])
 
     def _get_batch(self) -> jax.Array:
-        with latency.timer("Manager._get_batch"):
+        with latency.timer('Manager._get_batch'):
             return self.dataset.sample_tokens(
                 ntokens=self.conf.length,
                 batch=self.conf.batch,
@@ -380,26 +384,26 @@ class Manager(object):
         if steps is None:
             steps = INF
 
-        t = "" if time_limit is None else f" {time_limit} s"
-        s = "" if steps > 1e10 else f" {steps} steps"
+        t = '' if time_limit is None else f' {time_limit} s'
+        s = '' if steps > 1e10 else f' {steps} steps'
 
-        logging.info(f"Training for{t}{s}")
+        logging.info(f'Training for{t}{s}')
         deadline = INF
         soft_deadline = INF
         start_time = None
 
-        # Checking deadline, step limit and soft deadline when the number of
-        # steps is a power of 2.
-
         while self.step < steps:
             if perf_counter() > deadline:
                 logging.info(
-                    f"Stopped at step {self.step} due to hard time limit {ttoa3(time_limit)}"
+                    f'Stopped at step {self.step} due to hard time limit {ttoa3(time_limit)}'
                 )
                 break
-            if self.step & (self.step - 1) == 0 and perf_counter() > soft_deadline:
+            if (
+                self.step & (self.step - 1) == 0
+                and perf_counter() > soft_deadline
+            ):
                 logging.info(
-                    f"Stopped at step {self.step} / {steps} due to soft time limit {ttoa3(soft_tl)}"
+                    f'Stopped at step {self.step} / {steps} due to soft time limit {ttoa3(soft_tl)}'
                 )
                 break
 
@@ -414,7 +418,7 @@ class Manager(object):
             step_end = perf_counter()
 
             if math.isnan(loss) or math.isinf(loss):
-                logging.warning(f"Loss is {loss}, stopping training")
+                logging.warning(f'Loss is {loss}, stopping training')
                 # Don't register the training time if it diverged.
                 start_time = None
                 break
@@ -425,7 +429,9 @@ class Manager(object):
                 or step_end - last_report > 10
             ):
                 last_report = step_end
-                logging.info(self.run.report_recent_loss(self.tokenizer.tokenset))
+                logging.info(
+                    self.run.report_recent_loss(self.tokenizer.tokenset)
+                )
 
             if (
                 temp_steps is not None
@@ -440,7 +446,9 @@ class Manager(object):
                 deadline = start_time + time_limit if time_limit else INF
                 soft_deadline = start_time + soft_tl if soft_tl else INF
 
-        total_time = None if start_time is None else perf_counter() - start_time
+        total_time = (
+            None if start_time is None else perf_counter() - start_time
+        )
 
         return total_time, self.conf.replace(steps=self.step)
 
@@ -483,12 +491,12 @@ class Manager(object):
             if math.isnan(eval_loss):
                 eval_loss = INF
         except TrainingDiverged:
-            logging.warning("Training stopped early.")
+            logging.warning('Training stopped early.')
             eval_loss = INF
 
         self.run.finalize(eval_loss, train_time)
         logging.info(
-            f"{self.conf}:  loss {eval_loss:.4f} b/byte  T = {ttoa3(train_time)}"
+            f'{self.conf}:  loss {eval_loss:.4f} b/byte  T = {ttoa3(train_time)}'
         )
 
         return (self.run, self.weights, final_conf)
@@ -502,7 +510,11 @@ class Manager(object):
         out = self.sample(prefix_tokens, length, temperature)
         full_text = list(prefix_tokens) + out
 
-        out = self.tokenizer.untokenize(full_text) if self.tokenizer else bytes(out)
+        out = (
+            self.tokenizer.untokenize(full_text)
+            if self.tokenizer
+            else bytes(out)
+        )
 
         return out
 
@@ -526,5 +538,5 @@ class Manager(object):
 
         if self.pre_training:
             for stage in self.pre_training:
-                total_steps += stage["step"]
-        return f"{model_name}-{self.step}"
+                total_steps += stage['step']
+        return f'{model_name}-{self.step}'

a2790cde611929f8515f944c39ad96506cfbfa6e
Oleg Eterevsky <oleg@eterevsky.com>
2025-10-22 09:27:21 +0200

    Tests for bits.2

--- texmo/layers/input_bits_test.py
+++ texmo/layers/input_bits_test.py
@@ -73,3 +73,42 @@ def test_bits1_pos_batch():
                     [1, 0, 1, 0, 0, 0, 0, 0, 0],
                     [1, 0, 0, 1, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 1, 0, 0, 0, 0]]]))
+
+
+def test_bits2():
+    layer = InputBits.from_spec('bits.2')
+
+    assert layer.ntokens == 4
+    assert layer.output_size == 2
+    assert layer.tokens_name == 'bits.2'
+    assert str(layer) == 'bits.2'
+    assert layer.is_valid()
+    state = layer.init_state(None, jnp.float32)
+
+    _, encoding = layer.step(None, state, 1, jnp.float32)
+    np.testing.assert_array_equal(encoding, jnp.array([1, 0]))
+
+    _, encoding = layer.step(None, state, 2, jnp.float32)
+    np.testing.assert_array_equal(encoding, jnp.array([0, 1]))
+
+
+def test_bits2_batch():
+    layer = InputBits.from_spec('bits.2')
+
+    # batch: 2 len: 4
+    input = jnp.array([[0, 1, 2, 3], [2, 3, 0, 1]])
+
+    output = layer.forward_batch(None, input, 1, dtype=jnp.float32)
+
+    np.testing.assert_array_equal(
+        output,
+        jnp.array([[[0, 0],
+                    [0, 0],
+                    [1, 0],
+                    [0, 1],
+                    [1, 1]],
+                   [[0, 0],
+                    [0, 1],
+                    [1, 1],
+                    [0, 0],
+                    [1, 0]]]))

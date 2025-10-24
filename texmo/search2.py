888777d454c5e058af3271f1bc1262d4981c3edb
Oleg Eterevsky <oleg@eterevsky.com>
2025-10-24 14:30:22 +0200

    Pretty print time limit

--- texmo/search2.py
+++ texmo/search2.py
@@ -264,13 +264,13 @@ class Search(object):
 
             conf = self._select_untimed(t, max_weights, system)
             if conf is not None:
-                console.log(f'Conf for {system}: {conf}, TL: {t}')
+                console.log(f'Conf for {system}: {conf}, TL: {ttoa3(t)}')
                 return conf, t
 
             conf = self._select_top_neighbor(t, max_weights, system)
             if conf is not None:
-                console.log(f'Conf for {system}: {conf}, TL: {t}')
+                console.log(f'Conf for {system}: {conf}, TL: {ttoa3(t)}')
                 return conf, t
 
-            console.log(f'Conf for {system}: {self._init_conf} (default), TL: {tmax}')
+            console.log(f'Conf for {system}: {self._init_conf} (default), TL: {ttoa3(tmax)}')
             return self._init_conf, tmax
